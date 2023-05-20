d9f997eb163e6212a192346b6b7037bf674f636a
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-05-20 10:21:49 -0700

    registers models on admin.py, checks server & db communication

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .utils import soup_data
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
 
-# Create your views here.
 @api_view(['GET'])
 def scrape_soup_data(request, ticker):
     data = soup_data(ticker)
     return Response(data)
+
+def home(request):
+  return HttpResponse('<h1>Welcome to SimuStock - home</h1>')
